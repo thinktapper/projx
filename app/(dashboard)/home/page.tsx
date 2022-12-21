f43b2c9ae86e980cb735e368d82c0563a589837e
Andrew Tapper <andrew@tapper.codes>
2022-12-21 13:48:59 -0600

    :sparkles: add project and tasks card components - update seed script to hash mock user password

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,13 +1,31 @@
 // import { delay } from '@/lib/async'
 import Greeting from '@/components/Greeting'
 import GreetingSkeleton from '@/components/GreetingSkeleton'
+import ProjectCard from '@/components/ProjectCard'
 import { getUserFromCookie } from '@/lib/auth'
 import { db } from '@/lib/db'
 import { cookies } from 'next/headers'
 import Link from 'next/link'
 import { Suspense } from 'react'
 
+const getData = async () => {
+  // await delay(2000)
+  const user = await getUserFromCookie(cookies())
+  const projects = await db.project.findMany({
+    where: {
+      ownerId: user?.id,
+    },
+    include: {
+      tasks: true,
+    },
+  })
+
+  return { projects }
+}
+
 export default async function Page() {
+  const { projects } = await getData()
+
   return (
     <div className="h-full overflow-y-auto pr-6 w-1/1">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
@@ -17,7 +35,13 @@ export default async function Page() {
           </Suspense>
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
-          {/** projects map here */}
+          {projects.map((project) => (
+            <div key={project.id} className="w-1/3 p-3">
+              <Link href={`/project/${project.id}`}>
+                <ProjectCard project={project} />
+              </Link>
+            </div>
+          ))}
           <div className="w-1/3 p-3">{/* new project here */}</div>
         </div>
         <div className="mt-6 flex-2 grow w-full flex">
